8129caddad9281242adaac71d01cc552122b324b
AsadullahQureshi <abduljabbarasadullah@gmail.com>
2023-03-12 13:16:42 +0500

    Changes App Events and data binding anf refs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Hello Vue:) {{ title }}</h1>
+  <p>My first Vue App</p>
+  <input type="text" v-model="title" ref="name">
+  <button @click="changeptag">Click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+    return{
+      title:"Asad",
+    }
+  },
+  methods: {
+    changeptag(){
+        this.title="Change Through Button"
+        console.log(this.$refs.name)
+        this.$refs.name.classList.add('form-control')
+        this.$refs.name.focus()
+    }
+
+
+  },
 }
 </script>
 
